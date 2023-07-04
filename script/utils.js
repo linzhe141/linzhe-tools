fd053ec35abbf907d4fd85164b37ba424c0c8115
zhengp <linzhe141@qq.com>
2023-07-04 18:01:58 +0800

    [feat] 打包新增terminal spinner

--- script/utils.js
+++ script/utils.js
@@ -1,6 +1,7 @@
 import { execa } from 'execa'
 import chalk from 'chalk'
 import dayjs from 'dayjs'
+import { createSpinner } from 'nanospinner'
 export const log = console.log
 
 export function formatTime() {
@@ -11,7 +12,9 @@ export async function runTask(tasks) {
   for (const task of tasks) {
     log(formatTime(), chalk.green(task.startMessage))
     try {
+      const spinner = createSpinner('building').start()
       await execa(task.command)
+      spinner.success()
       log(formatTime(), chalk.green(task.doneMessage))
     } catch (error) {
       log(formatTime(), chalk.red('失败 ' + error))
