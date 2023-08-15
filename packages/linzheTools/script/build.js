11bf7373b5c742024853c1bc3a474e51e41352c2
linzhe <linzhe141@qq.com>
2023-08-15 11:30:16 +0800

    [chore] 优化打包流程

--- packages/linzheTools/script/build.js
+++ packages/linzheTools/script/build.js
@@ -1,6 +1,7 @@
 import { build } from 'vite'
 import { copy, remove } from 'fs-extra'
 import chalk from 'chalk'
+import { buildTypes } from './buildTypes.js'
 async function runBuild() {
   try {
     await build()
@@ -8,6 +9,7 @@ async function runBuild() {
     await copy('./dist/es/style', './dist/style')
     await remove('./dist/es/style')
     console.log(chalk.green('style.css文件成功抽取到dist/style目录下!'))
+    buildTypes()
   } catch (error) {
     console.log(chalk.red('Vite build failed!'))
     console.log(chalk.red(error))
