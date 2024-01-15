11ffa17a787a5215675e328be8b69d7c94c30663
linzhe141 <linzhe141@qq.com>
2024-01-15 10:29:18 +0800

    chore: docs 新增 alias

---
+++ docs/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import { join } from 'path'
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: 'linzhe-tools/style.css',
+        replacement: join(
+          __dirname,
+          '..',
+          'packages/linzhe-tools/dist/style/style.css'
+        ),
+      },
+      {
+        find: 'linzhe-tools',
+        replacement: join(__dirname, '..', 'packages/linzhe-tools/index.ts'),
+      },
+      {
+        find: /^@linzhe-tools\/(.+)$/,
+        replacement: join(__dirname, '..', 'packages', '$1', 'index.ts'),
+      },
+    ],
+  },
+})
